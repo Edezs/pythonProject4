80a4bb2ce590cde874e8ed86a98edf6a5619c573
Gartenzwerg <lottokingede@web.de>
2022-07-11 23:25:59 +0200

    please dont touch

--- main.py
+++ main.py
@@ -1,42 +1,68 @@
-# This is a sample Python script.
-
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+from selenium.webdriver import Keys
 from selenium.webdriver.common.by import By
-import time
+from time import sleep
 from selenium import webdriver
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+from selenium.webdriver.support import ui
 
 
-# Press the green button in the gutter to run the script.
+def checkFoto(foto):
+    return
+
+
 if __name__ == '__main__':
 
-    driver = webdriver.Chrome('/Users/password1234/chromedriver')  # Optional argument, if not specified will search path.
+    driver = webdriver.Chrome('/Users/chromedriver')
+    driver.get('http://www.ebay-kleinanzeigen.de/')
+    sleep(1)
+    cookieAcceptBox = driver.find_element(By.XPATH, '//*[@id="gdpr-banner-accept"]')
+    cookieAcceptBox.click()
 
-    driver.get('http://www.ebay-kleinanzeigen.de/');
+    searchBox = driver.find_element(By.XPATH, '//*[@id="site-search-query"]')
+    searchBox.send_keys('stuhl')
+    searchBox.submit()
+    sleep(1)
+    #TODO: check if
+    anzeigen = driver.find_elements(By.CLASS_NAME, 'aditem-image')
 
-    time.sleep(1)  # Let the user actually see something!
+    for i in range(6):
+        anzeige = driver.find_element(By.CLASS_NAME, 'aditem-image')
+        print(anzeige.id)
+        driver.get('https://www.google.com?q=python#q=python')
+        sleep(2)
+        first_result = ui.WebDriverWait(driver, 15).until(lambda browser: browser.find_element_by_class_name('rc'))
+        first_link = first_result.find_element_by_tag_name('a')
 
-    # knopf = driver.find_element_by_xpath("/html/body/div[2]/div[2]/div[3]/span/div/div/div/div[3]/div[1]/button[1]/div")
-    Cookie_accept_box = driver.find_element(By.XPATH, '//*[@id="gdpr-banner-accept"]')
-    Cookie_accept_box.click()
-    #
-    time.sleep(1)  # Let the user actually see something!
-    # //*[@id="site-signin"]/nav/ul/li[3]/a
+        # Save the window opener (current window, do not mistaken with tab... not the same)
+        main_window = driver.current_window_handle
 
-    search_box = driver.find_element(By.XPATH, '//*[@id="site-search-query"]')
-    search_box.send_keys('stuhl')
-    search_box.submit()
-    time.sleep(1)
+        # Open the link in a new tab by sending key strokes on the element
+        # Use: Keys.CONTROL + Keys.SHIFT + Keys.RETURN to open tab on top of the stack
+        first_link.send_keys(Keys.CONTROL + Keys.RETURN)
 
-    Anzeige = driver.find_element(By.TAG_NAME, "li")
-    print(Anzeige.tag_name)
-    #time.sleep(2)
-    #if (driver.find_element(By.CLASS_NAME)=="aditem"):
-    Anzeige.click()
+        # Switch tab to the new tab, which we will assume is the next one on the right
+        driver.find_element_by_tag_name('body').send_keys(Keys.CONTROL + Keys.TAB)
+
+        # Put focus on current window which will, in fact, put focus on the current visible tab
+        driver.switch_to_window(main_window)
+
+        # do whatever you have to do on this page, we will just got to sleep for now
+        sleep(2)
+
+        # Close current tab
+        driver.find_element_by_tag_name('body').send_keys(Keys.CONTROL + 'w')
+
+        # Put focus on current window which will be the window opener
+        driver.switch_to_window(main_window)
+     #   print(anzeige.id)
+      #  #anzeige.click()
+       # #time.sleep(1)
+        ##driver.back()
+        #driver.get("https://www.tutorialspoint.com/questions/index.php")
+      #  print("Current Page title: " + driver.title)
+       # driver.back()
+
+
+    #option 1, immer die erste anzeige prüfen
 
     #pseudocode für einen Suchbegriff
     #anzeige finden
